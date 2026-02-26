854b9c5c2d1088df494a64446c2ed24d7ef23578
Owaish777 <mohammedowaishtanwer@gmail.com>
2026-02-26 14:57:16 +0530

    Add water based on mesh deformation

---
+++ Assets/Scripts/SimpleWave.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class SimpleWave : MonoBehaviour
+{
+    public float amplitude = 0.5f;
+    public float wavelength = 2f;
+    public float speed = 1f;
+
+    private Mesh mesh;
+    private Vector3[] baseVertices;
+    private Vector3[] vertices;
+
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        baseVertices = mesh.vertices;
+        vertices = new Vector3[baseVertices.Length];
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < baseVertices.Length; i++)
+        {
+            Vector3 vertex = baseVertices[i];
+
+            float wave = Mathf.Sin(
+                (vertex.x + Time.time * speed) / wavelength
+            ) * amplitude;
+
+            vertex.y = wave;
+            vertices[i] = vertex;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+}
