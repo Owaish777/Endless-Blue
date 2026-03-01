0ed1753a3e6d633d74cb3f9533eeebb531719acc
Owaish777 <mohammedowaishtanwer@gmail.com>
2026-03-01 15:11:57 +0530

    Improve stability of the boat

--- Assets/Scripts/SimpleBuoyancy.cs
+++ Assets/Scripts/SimpleBuoyancy.cs
@@ -2,19 +2,31 @@ using UnityEngine;
 
 public class SimpleBuoyancy : MonoBehaviour
 {
-    public Transform[] floatPoints;
+    public GameObject buoyancyPoints;
     public GameObject water;
+    public Transform centerOfMassTransform;
     public float floatForce = 10f;
     public float damping = 0.5f;
     public float angularDamping = 1f;
+    public float maxSubmersionDepth = 2f;
+    public float stabilityStrength = 1f;
 
     private Rigidbody rb;
     private SimpleWave simpleWave;
+    private Transform[] floatPoints;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = transform.InverseTransformPoint(centerOfMassTransform.position);
+
         simpleWave = water.GetComponent<SimpleWave>();
+
+        floatPoints = new Transform[buoyancyPoints.transform.childCount];
+        for (int i = 0; i < buoyancyPoints.transform.childCount; i++)
+        {
+            floatPoints[i] = buoyancyPoints.transform.GetChild(i);
+        }
     }
 
     void FixedUpdate()
@@ -26,9 +38,15 @@ public class SimpleBuoyancy : MonoBehaviour
 
             if (diff > 0)
             {
-                rb.AddForceAtPosition(Vector3.up * floatForce * diff, point.position);
+                float submersion = Mathf.Clamp01(diff / maxSubmersionDepth);
+
+                rb.AddForceAtPosition(Vector3.up * floatForce * submersion, point.position);
             }
         }
+
+        //Upright Stabilization Torque
+        Vector3 rightingTorque = Vector3.Cross(transform.up, Vector3.up);
+        rb.AddTorque(rightingTorque * stabilityStrength);
 
         rb.AddForce(-rb.linearVelocity * damping);
         rb.AddTorque(-rb.angularVelocity * angularDamping);
